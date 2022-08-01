f66c532911c1e5d48776aed8d40557126ce4400c
SergeyRO <sergey_r.o@mail.ru>
2022-08-01 22:12:10 +0300

    two commit

--- main.py
+++ main.py
@@ -6,34 +6,32 @@ import csv
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-pprint(contacts_list)
 
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
-print(f'/n/n/n/n//n/n')
-i = 0
 contacts_list_modify = list()
 for item in contacts_list:
-    if i == 0:
-        contacts_list_modify.append(item)
-    else:
-        contacts_list_modify.append(item)
-        pattern = r"(\+7|8)(\s|\()?(\()?(\d{3})(\s|\))?(\s|\-)?(\d{3})(\s|\-)?(\d{2})(\s|\-)?(\d{2})"
-        st = item
-        contacts_list_modify.append(re.sub(pattern, r"+7(\4)\7 -\9 -\11", st))
+    pattern = r"(\+7|8)(\s|\()*(\()*(\d{3})(\s|\))*(\s|\-)*(\d{3})(\s|\-)*(\d{2})(\s|\-)*(\d{2})(\(|\s)*(\(|\s)*(доб.)*(\s)*(\d+)*(\s|\))*"
+    contacts_list_modify.append((re.sub(pattern, r'+7(\4)\7-\9-\11\15\14\16', ','.join(item))).split(','))
 
-        # pattern = r'(\+7|8)(\s|\()?(\()?(\d{3})(\s|\))?(\s|\-)?(\d{3})(\s|\-)?(\d{2})(\s|\-)?(\d{2})(\(|\s)?(\(|\s)?(доб.)(\s|)?(\d+)(\s|\))?'
-        # replace1 = r'+7(\4)\7 -\9 -\11 \14\16'
-        # print(re.sub(pattern, replace1, string))
-    i += 1
-print(contacts_list_modify)
+contacts_list = list()
+for items in contacts_list_modify:
+    pattern = r'^([А-ЯЁа-яё]*)(\s*)(\,?)([А-ЯЁа-яё]*)(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
+    contacts_list.append((re.sub(pattern, r'\1\3\10\4\6\9\7\8', ','.join(items))).split(','))
+
+pprint(contacts_list)
+print('=======================================================================')
+list_cont = list()
+for i in contacts_list:
+    if i not in list_cont:
+        
+        list_cont.append(i)
+
+print('---------------------------------------------------------------------')
+pprint(list_cont)
 # # TODO 2: сохраните получившиеся данные в другой файл
 # # код для записи файла в формате CSV
 # with open("phonebook.csv", "w") as f:
 #     datawriter = csv.writer(f, delimiter=',')
 #     # Вместо contacts_list подставьте свой список
 #     datawriter.writerows(contacts_list)
-
-string = 'foo42buz'
-pattern = re.compile(r'(\d+)buz')
-print(re.sub(pattern,'bar',string))
