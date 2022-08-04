0a660e08a99d0fca4639fd1beec6fcfc6d4c6571
SergeyRO <sergey_r.o@mail.ru>
2022-08-04 22:18:02 +0300

    finish commit

--- main.py
+++ main.py
@@ -19,19 +19,32 @@ for items in contacts_list_modify:
     pattern = r'^([А-ЯЁа-яё]*)(\s*)(\,?)([А-ЯЁа-яё]*)(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
     contacts_list.append((re.sub(pattern, r'\1\3\10\4\6\9\7\8', ','.join(items))).split(','))
 
-pprint(contacts_list)
-print('=======================================================================')
-list_cont = list()
+for a in contacts_list:
+    for b in contacts_list:
+        if a[0] == b[0]:
+            if a is not b:
+                if a[1] == '':
+                    a[1] = b[1]
+                if a[2] == '':
+                    a[2] = b[2]
+                if a[3] == '':
+                    a[3] = b[3]
+                if a[4] == '':
+                    a[4] = b[4]
+                if a[5] == '':
+                    a[5] = b[5]
+                if a[6] == '':
+                    a[6] = b[6]
+l=list()
 for i in contacts_list:
-    if i not in list_cont:
-        
-        list_cont.append(i)
+    if i not in l:
+        l.append(i)
 
-print('---------------------------------------------------------------------')
-pprint(list_cont)
-# # TODO 2: сохраните получившиеся данные в другой файл
-# # код для записи файла в формате CSV
-# with open("phonebook.csv", "w") as f:
-#     datawriter = csv.writer(f, delimiter=',')
-#     # Вместо contacts_list подставьте свой список
-#     datawriter.writerows(contacts_list)
+contacts_list = l
+
+# TODO 2: сохраните получившиеся данные в другой файл
+# код для записи файла в формате CSV
+with open("phonebook.csv", "w", encoding='utf-8') as f:
+    datawriter = csv.writer(f, delimiter=',')
+    # Вместо contacts_list подставьте свой список
+    datawriter.writerows(contacts_list)
